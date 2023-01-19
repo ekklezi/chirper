151bf3003979381e4c085ca76ed78f93382d195e
ekklezi <akleziast@gmail.com>
2023-01-19 19:24:07 +0300

    Notification & Event

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -2,7 +2,9 @@
 
 namespace App\Notifications;
 
+use App\Models\Chirp;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -16,9 +18,8 @@ class NewChirp extends Notification
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
-        //
     }
 
     /**
@@ -41,9 +42,11 @@ class NewChirp extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->subject("New Chirp from {$this->chirp->user->name}")
+            ->greeting("New Chirp from {$this->chirp->user->name}")
+            ->line(Str::limit($this->chirp->message, 50))
+            ->action('Go to Chirper', url('/'))
+            ->line('Thank you for using our application!');
     }
 
     /**
